15e686e30ca4d9c2ec5d4c4c35fbe4e2e25b8c99
Vineet Jadhav <vineet290403@gmail.com>
2024-03-31 23:04:00 +0530

    Added Monorepo with express nextjs

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -1,17 +1,32 @@
-import { getServerSession } from "next-auth"
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 import { authOptions } from "../../lib/auth";
 
 export const GET = async () => {
-    const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
+
+  try {
     if (session.user) {
-        return NextResponse.json({
-            user: session.user
-        })
+      return NextResponse.json({
+        user: session.user,
+      });
     }
-    return NextResponse.json({
-        message: "You are not logged in"
-    }, {
-        status: 403
-    })
-}
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "You are not logged in",
+      },
+      {
+        status: 403,
+      }
+    );
+  }
+  return NextResponse.json(
+    {
+      message: "You are not logged in",
+    },
+    {
+      status: 403,
+    }
+  );
+};
